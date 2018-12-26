da281f31ec004e80492ffd416ecbc0ec48702681
= <andjm@andywithrice@gmail.com>
2018-12-27 01:37:53 +0700

    cycling python code

--- bluetoothServer.py
+++ bluetoothServer.py
@@ -13,89 +13,92 @@ def change_DPI():
 def changeTime():
     call(["./changeTime.sh"])
     return
-    
 
+def doBlue():
+    server_sock=BluetoothSocket( RFCOMM )
+    server_sock.bind(("",PORT_ANY))
+    server_sock.listen(1)
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
-server_sock.listen(1)
+    port = server_sock.getsockname()[1]
 
-port = server_sock.getsockname()[1]
+    uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
-uuid = "00001101-0000-1000-8000-00805F9B34FB"
+    advertise_service( server_sock, "ahihi",
+                    service_id = uuid,
+                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                    profiles = [ SERIAL_PORT_PROFILE ], 
+    #                   protocols = [ OBEX_UUID ] 
+                        )
+                    
+    print("Waiting for connection on RFCOMM channel %d" % port)
 
-advertise_service( server_sock, "ahihi",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-                   
-print("Waiting for connection on RFCOMM channel %d" % port)
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
-
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-        print(type(data))
-        if(str(data) == "browsefile"):
-            print("browsefile")
-            from os import listdir
-            from os.path import isfile, join
-            mypath = 'images/'
-            onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-            stringlist = " ".join(onlyfiles)
-            stringlist = "filelist " + stringlist
-            print(stringlist)
-            client_sock.send(stringlist) 
-        elif(data=="checkinfo_123456"):        
-            dataDaily=''
-            myfile=open('/home/pi/test.txt', 'r') 
-            dataDaily=myfile.read()
-            print(str(dataDaily))
-            dataDaily = "checkinfo "+str(dataDaily)
-            client_sock.send(dataDaily)
-        else:
-            data = str(data).split(" ")
-            if(data[0]=="transfer"):
-                print("transfering..."+data[1])
-                fname = 'images/'+data[1]
-                from PIL import Image
-                im = Image.open(fname)
-                im.save(fname, dpi=(100,100))
-                with open(fname,'rb') as imageFile:
-                    strng = imageFile.read()
-                    #strng = bytearray(strng)
-                    client_sock.send(strng)
-                    print(strng)
-                    print(data[1]+' sent!')
-            elif(data[0]=="DPI"):
-                dpifile = open("outputDPI.txt","w")
-                dpifile.write(data[1])
-                dpifile.close()
-                thread_changeDPI = Thread(target = change_DPI)
-                thread_changeDPI.start()
-                thread_changeDPI.join()
-            elif(data[0] == "Time"):
-                tempList = data[1].split("-")
-                minute = tempList[len(tempList)-1]
-                hour   = tempList[len(tempList)-2]
-                time   = hour + " " + minute
-                fileTime = open('outputTime.txt','w')
-                fileTime.write(time)
-                fileTime.close()
-                thread_changeTime = Thread(target = changeTime)
-                thread_changeTime.start()
-                thread_changeTime.join()
-        #process_message(str(data))
-        #server_sock.send("hello again")
-except Exception as e:
-    print(e)
-    pass
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            print("received [%s]" % data)
+            print(type(data))
+            if(str(data) == "browsefile"):
+                print("browsefile")
+                from os import listdir
+                from os.path import isfile, join
+                mypath = 'images/'
+                onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+                stringlist = " ".join(onlyfiles)
+                stringlist = "filelist " + stringlist
+                print(stringlist)
+                client_sock.send(stringlist) 
+            elif(data=="checkinfo_123456"):        
+                dataDaily=''
+                myfile=open('/home/pi/test.txt', 'r') 
+                dataDaily=myfile.read()
+                print(str(dataDaily))
+                dataDaily = "checkinfo "+str(dataDaily)
+                client_sock.send(dataDaily)
+            else:
+                data = str(data).split(" ")
+                if(data[0]=="transfer"):
+                    print("transfering..."+data[1])
+                    fname = 'images/'+data[1]
+                    from PIL import Image
+                    im = Image.open(fname)
+                    im.save(fname, dpi=(100,100))
+                    with open(fname,'rb') as imageFile:
+                        strng = imageFile.read()
+                        #strng = bytearray(strng)
+                        client_sock.send(strng)
+                        print(strng)
+                        print(data[1]+' sent!')
+                elif(data[0]=="DPI"):
+                    dpifile = open("outputDPI.txt","w")
+                    dpifile.write(data[1])
+                    dpifile.close()
+                    thread_changeDPI = Thread(target = change_DPI)
+                    thread_changeDPI.start()
+                    thread_changeDPI.join()
+                elif(data[0] == "Time"):
+                    tempList = data[1].split("-")
+                    minute = tempList[len(tempList)-1]
+                    hour   = tempList[len(tempList)-2]
+                    time   = hour + " " + minute
+                    fileTime = open('outputTime.txt','w')
+                    fileTime.write(time)
+                    fileTime.close()
+                    thread_changeTime = Thread(target = changeTime)
+                    thread_changeTime.start()
+                    thread_changeTime.join()
+            #process_message(str(data))
+            #server_sock.send("hello again")
+    except Exception as e:
+        print(e)
+        client_sock.close()
+        server_sock.close()
+        doBlue()
+        pass
+doBlue()
 
 print("disconnected")
 
@@ -105,8 +108,7 @@ def process_message(message):
         os.system("sudo apt-get update")
     return 
 
-client_sock.close()
-server_sock.close()
+
 print("all done")
 
 if __name__ == "__main__":
